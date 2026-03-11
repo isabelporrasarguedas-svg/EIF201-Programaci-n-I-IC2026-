71eff27a552040bc6770795c4068ac463aa5d6ef
Isabel Patricia Porras Arguedas <isabel.porras.arguedas@est.una.ac.cr>
2026-03-11 13:21:26 -0600

    paqueteria.h

---
+++ sesion03/paqueteria-labo02-/paqueteria.h
@@ -0,0 +1,18 @@
+#ifndef PAQUETERIA_H
+#define PAQUETERIA_H
+
+
+double* crearRegristro(int& cant);
+
+void IngresarPeso(double* pesos, int cant)
+
+double calcularPesoTotal(const double* pesos, int cant);
+
+
+int sobreLimite(const double* pesos, int cant, double limites);
+
+const double*
+buscaElmasPesado(const double* pesos, int cantidad);
+
+
+#endif
