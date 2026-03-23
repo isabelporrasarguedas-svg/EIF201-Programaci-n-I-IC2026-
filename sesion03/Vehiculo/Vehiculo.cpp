bd81c73577d39625d5b7559212cffb70a71da5a8
Isabel Patricia Porras Arguedas <isabel.porras.arguedas@est.una.ac.cr>
2026-03-23 13:59:30 -0600

    labo03 arreglo cpp

--- sesion03/Vehiculo/Vehiculo.cpp
+++ sesion03/Vehiculo/Vehiculo.cpp
@@ -1,12 +1,30 @@
-
 #include "Vehiculo.h"
 #include <iostream>
 
 namespace UNA {
 
-    int main()
-    {
-        std::cout << "\n";
+    Vehiculo::Vehiculo(std::string placa, std::string marca, int anio, double km)
+        : placa(placa), marca(marca), anio(anio), kilometraje(km), activo(true) {
     }
 
+    std::string Vehiculo::getPlaca() const { return placa; }
+    std::string Vehiculo::getMarca() const { return marca; }
+    int Vehiculo::getAnio() const { return anio; }
+    double Vehiculo::getKilometraje() const { return kilometraje; }
+    bool Vehiculo::isActivo() const { return activo; }
+
+
+    void Vehiculo::registrarKilometros(double km)
+    {
+
+    }
+    void Vehiculo::desactivar()
+    {
+    }
+    void Vehiculo::reactivar()
+    {
+    }
+    void Vehiculo::mostrar() const
+    {
+    }
 }
